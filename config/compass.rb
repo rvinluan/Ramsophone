68fb4ec15eef37b5d7542fb14f1c100e380aae9a
Robert Vinluan <robert.vinluan@nytimes.com>
2016-04-09 00:46:22 -0400

    restructure files

--- config/compass.rb
+++ config/compass.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
+css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
